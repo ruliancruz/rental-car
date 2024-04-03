4d371e2a9c7aae7e58e469745dcc05bf1c895f93
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-04-03 12:35:38 -0300

    queiry files organization fixed

--- queries/03.sql
+++ queries/03.sql
@@ -16,19 +16,3 @@ INSERT INTO CAR_MODELS (MODEL_NAME) VALUES
   ('Minivan'),
   ('Utilitário'),
   ('Buggy');
-
-CREATE TABLE CAR_BRANDS
-(
-  ID INTEGER PRIMARY KEY AUTOINCREMENT,
-  BRAND_NAME VARCHAR(32) NOT NULL
-);
-
-INSERT INTO CAR_BRANDS (BRAND_NAME) VALUES
-  ('Chevrolet'),
-  ('Toyota'),
-  ('Hyundai'),
-  ('Volkswagen'),
-  ('Jeep'),
-  ('Renault'),
-  ('Honda'),
-  ('Fiat');
--- queries/04.sql
+++ queries/04.sql
@@ -1,32 +1,16 @@
 -- SQLite
-CREATE TABLE POSITIONS
+CREATE TABLE CAR_BRANDS
 (
   ID INTEGER PRIMARY KEY AUTOINCREMENT,
-  DESCRIPTION VARCHAR(120) NOT NULL
+  BRAND_NAME VARCHAR(32) NOT NULL
 );
 
-INSERT INTO POSITIONS (DESCRIPTION) VALUES
-  ('Gerente de vendas'),
-  ('Gerente de compras'),
-  ('Vendedor'),
-  ('Mecânico'),
-  ('Assistente Administrativo');
-
-CREATE TABLE EMPLOYEES
-(
-  ID INTEGER PRIMARY KEY AUTOINCREMENT,
-  NAME VARCHAR(100) NOT NULL,
-  PHONE VARCHAR(16) NOT NULL,
-  CONTRACT_DATE DATETIME NOT NULL,
-  POSITION_ID INTEGER NOT NULL,
-
-  FOREIGN KEY (POSITION_ID) REFERENCES POSITIONS(ID)
-);
-
-INSERT INTO EMPLOYEES (NAME, PHONE, CONTRACT_DATE, POSITION_ID) VALUES
-  ('Adriana Lemes',	'(11) 98789-9999',	'10/04/2021',	1),
-  ('Camila Soares',	'(11) 92749-9599',	'25/03/2008',	3),
-  ('Leonardo Silva',	'(11) 91449-9600',	'26/08/2018',	5),
-  ('Mayara Gomes',	'(11) 92649-9601',	'18/07/2016',	2),
-  ('Matheus Alves',	'(11) 92749-9602',	'04/03/2015',	5),
-  ('Aline Santos',	'(11) 99789-3135',	'06/05/2021',	3);
+INSERT INTO CAR_BRANDS (BRAND_NAME) VALUES
+  ('Chevrolet'),
+  ('Toyota'),
+  ('Hyundai'),
+  ('Volkswagen'),
+  ('Jeep'),
+  ('Renault'),
+  ('Honda'),
+  ('Fiat');
